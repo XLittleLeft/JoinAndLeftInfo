1a4ad37bca172b58b62ee9ac440a65a5d02d9252
XLittleLeft <XLittleLeft@Gmail.com>
2023-02-12 21:28:38 +0800

    添加项目文件。

---
+++ JoinAndLeftInfo/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoinAndLeftInfo
+{
+    public class Config
+    {
+        [Description("enable plugin")]
+        public bool IsEnabled { get; set; } = true;
+        [Description("player left info")]
+        public bool PlayerLeftInfo { get; set; } = true;
+        [Description("player joined info")]
+        public bool PlayerJoinedInfo { get; set; } = true;
+    }
+}
---
+++ JoinAndLeftInfo/MainClass.cs
@@ -0,0 +1,39 @@
+﻿using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Enums;
+using PluginAPI.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoinAndLeftInfo
+{
+    public class MainClass
+    {
+        [PluginEntryPoint("JoinAndLeftInfo","1.0.0","Show player join and left info in localadmin","XLittleleft")]
+        void OnLoad()
+        {
+            if (!Config.IsEnabled) return;
+            EventManager.RegisterEvents(this);
+        }
+
+        [PluginEvent(ServerEventType.PlayerJoined)]
+        void OnPlayerJoined(Player player)
+        {
+            if (Config.PlayerJoinedInfo)
+            Log.Info($"{player.Nickname} Has Joined Server,Server Player Count: {Server.PlayerCount + 1}");
+        }
+
+        [PluginEvent(ServerEventType.PlayerLeft)]
+        void OnPlayerLeft(Player player)
+        {
+            if (Config.PlayerLeftInfo)
+            Log.Info($"{player.Nickname} Has Left Server,Server Remaining Player Count: {Server.PlayerCount - 1}");
+        }
+
+        [PluginConfig]
+        public Config Config;
+    }
+}
